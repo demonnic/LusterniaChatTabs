eebbe3a6ed7bfe5ba45951580026b03736b81845
Damian Monogue <demonnic@gmail.com>
2021-02-27 19:44:23 -0500

    First release

---
+++ src/aliases/LusterniaChatTabs/lct.lua
@@ -0,0 +1,58 @@
+local valid_options = {"font", "fontSize", "blink", "blankLine", "timestamp", "save", "load"}
+local valid_options_string = table.concat(valid_options)
+local option = matches[2]
+local value = matches[3]
+if not table.contains(valid_options, option) then
+  cecho("<green>LusterniaChatTabs: <reset>Tried to change a config value that does not exist: valid options are " .. valid_options_string .. "\n")
+  return
+end
+local chatEMCO = LusterniaChatTabs.chatEMCO
+local EMCO = require("LusterniaChatTabs.emco")
+if option == "save" then
+  chatEMCO:save()
+elseif option == "load" then
+  chatEMCO:load()
+else
+  if value == nil then
+    cecho("<green>LusterniaChatTabs: <reset>You need to provide a value when trying to configure things\n")
+    return
+  end
+  if option == "font" then
+    local validFont = table.contains(getAvailableFonts(), value)
+    if not validFont then
+      cecho("<green>LusterniaChatTabs: <reset>You tried to set the font to one that Mudlet doesn't recognize. This will not do.")
+    else
+      chatEMCO:setFont(value)
+    end
+  elseif table.contains({"blink", "blankLine", "timestamp"}, option) then
+    value = EMCO:fuzzyBoolean(value)
+    if value then
+      if option == "timestamp" then
+        chatEMCO:enableTimestamp()
+      elseif option == "blankLine" then
+        chatEMCO:enableBlankLine()
+      elseif option == "blink" then
+        chatEMCO:enableBlink()
+      elseif option == "commandLine" then
+        chatEMCO:enableCommandLine()
+      end
+    else
+      if option == "timestamp" then
+        chatEMCO:disableTimestamp()
+      elseif option == "blankLine" then
+        chatEMCO:disableBlankLine()
+      elseif option == "blink" then
+        chatEMCO:disableBlink()
+      elseif option == "commandLine" then
+        chatEMCO:disableCommandLine()
+      end
+    end
+  else
+    value = tonumber(value)
+    if value then
+      chatEMCO:setFontSize(value)
+    else
+      cecho("<green>LusterniaChatTabs: <reset>You tried to set the fontSize to a nonnumber. Attempted: " .. matches[3] .. "\n")
+    end
+  end
+end
---
+++ src/scripts/LusterniaChatTabs/ChatTabs.lua
@@ -0,0 +1,103 @@
+local EMCO = require("LusterniaChatTabs.emco")
+LusterniaChatTabs = LusterniaChatTabs or {}
+LusterniaChatTabs.emcoContainer = LusterniaChatTabs.emcoContainer or Adjustable.Container:new({
+  name = "LusterniaChatTabsContainer",
+  x = "-25%",
+  y = "-60%",
+  width = "25%",
+  height = "60%",
+  attached = "right",
+})
+LusterniaChatTabs.chatEMCO = LusterniaChatTabs.chatEMCO or EMCO:new({
+  name = "LusterniaChat",
+  x = 0,
+  y = 0,
+  height = "100%",
+  width = "100%",
+  consoles = {"All", "Local", "Org", "Guild", "Tells", "Squad", "Order", "Clans", "Misc"},
+  allTab = true,
+  allTabName = "All",
+  blankLine = true,
+  blink = true,
+  bufferSize = 10000,
+  deleteLines = 500,
+  timestamp = true,
+  fontSize = 12,
+  font = "Inconsolata",
+  commandLine = true,
+}, LusterniaChatTabs.emcoContainer)
+local chatEMCO = LusterniaChatTabs.chatEMCO
+local filename = getMudletHomeDir() .. "/EMCO/LusterniaChat.lua"
+if io.exists(filename) then
+  chatEMCO:load()
+end
+chatEMCO:replayAll(10)
+
+local channelToTab = {
+  ct = "Org",
+  cgt = "Org",
+  gt = "Guild",
+  gts = "Guild",
+  gnt = "Guild",
+  sqt = "Squad",
+  tell = "Tells",
+  says = "Local",
+  emotes = "Local",
+  ot = "Order",
+  oto = "Order",
+  ft = "Misc",
+  newbie = "Misc",
+  market = "Misc",
+}
+
+local function addNDBdecho(txt)
+  local format = string.match(txt, "<%d+.->")
+  local names = ndb.findnames(txt)
+  if not names then
+    return txt
+  end
+  local done = {}
+  for _, name in pairs(names) do
+    if not done[name] then
+      local color = ndb.getcolor(name) or "<white>"
+      color = color:gsub("<", "")
+      color = color:gsub(">", "")
+      color = string.format("<%d,%d,%d>", unpack(color_table[color]))
+      txt = txt:gsub(name, color .. name .. format)
+      done[name] = true
+    end
+  end
+  return txt
+end
+
+local function chatCapture()
+  local info = gmcp.Comm.Channel.Text
+  local channel = "Misc"
+  if info.channel:starts("tell ") then
+    channel = "Tells"
+  end
+  if info.channel:match("clt") then
+    channel = "Clans"
+  end
+  if channelToTab[info.channel] then
+    channel = channelToTab[info.channel]
+  end
+  local txt = ansi2decho(info.text)
+  if ndb then
+    txt = addNDBdecho(txt)
+  end
+  chatEMCO:decho(channel, txt)
+end
+if LusterniaChatTabs.chatCaptureEventID then
+  killAnonymousEventHandler(LusterniaChatTabs.chatCaptureEventID)
+end
+LusterniaChatTabs.chatCaptureEventID = registerAnonymousEventHandler("gmcp.Comm.Channel.Text", chatCapture)
+local function startGMCPChat()
+  cecho("<green>LusterniaChatTabs:<reset> Turning on GMCP comm channels\n")
+  sendGMCP([[Core.Supports.Add ["Comm.Channel 1"] ]])
+  send("\n")
+  cecho("<green>LusterniaChatTabs:<reset> GMCP comm channels should now be enabled\n")
+end
+if not LusterniaChatTabs.loginTrig then
+  LusterniaChatTabs.loginTrig = tempExactMatchTrigger("Password correct. Welcome to Lusternia.", startGMCPChat, 1)
+end
